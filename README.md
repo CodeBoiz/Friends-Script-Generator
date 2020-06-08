c20fbe6d1174120b4b6429db8edf28c2bd05db83
Aaron Paul Jones <jonesaaron993@gmail.com>
2020-06-07 21:56:47 -0500

    Added two python files, one for webscrapping and getting the data of all Friends scritps, and the other being the python file to create the dataset, generate the model, then generate the output.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Friends-Script-Generator
 Generate a whole Friends using a neural network
+
+In order to get the data set, I run the python script `getDataset.py` to get all text from multiple webpages and write the text to a file.
+
+**Current State:** Training works fine, and will generate data after it is done, but saving and loading back a model is broken.
+
+To train a model, run this bat file:
+
+`train.bat` or `python model.py train`
+
+(Currently Broken) To generate text data, run this bat file:
+
+`generate.bat` or `python model.py generate --weights="model.h5"`
+
+
---
+++ getDataset.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    This python scipt is used as a webscraper to gather all Friends scripts through vairous links
+    found at this website https://fangj.github.io/friends/
+"""
+
+# Import Packages
+import httplib2
+from bs4 import BeautifulSoup, SoupStrainer
+import urllib.request
+import os
+
+# root directory of the project
+root_path = os.path.dirname(os.path.realpath(__file__))
+
+# Declare an http object
+http = httplib2.Http()
+
+# Get the status and respose from the webpage
+status, response = http.request('https://fangj.github.io/friends/')
+
+# Create a list
+links = []
+
+# (same directory) in append mode and 
+file1 = open(root_path + "/dataset/dataset.txt","a", encoding="utf-8") 
+
+# Open the webpage, declaring the right decoder and parser
+parser = 'html.parser'  # or 'lxml' (preferred) or 'html5lib', if installed
+resp = urllib.request.urlopen("https://fangj.github.io/friends/")
+soup = BeautifulSoup(resp, parser, from_encoding=resp.info().get_param('charset'))
+
+# Add hyperlinks to a list
+for link in soup.find_all('a', href=True):
+    links.append("https://fangj.github.io/friends/" + link['href'])
+
+# Open each webpage, read all text, then write the text to a file
+for i in range(0, len(links)):
+    resp = urllib.request.urlopen(links[i])
+    print("Reading " + str(resp))
+    soup = BeautifulSoup(resp, features="lxml")
+    txt = soup.get_text()
+    file1.write(txt)
+
+# Close the file
+file1.close()
